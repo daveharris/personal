894698959324927804e23a623c27aa0b7cfff488
Dave Harris <dave@boost.co.nz>
2012-05-28 10:35:18 +1200

    Fixed symlink

--- ruby/maths_dsl.rb
+++ ruby/maths_dsl.rb
@@ -1,1 +1,44 @@
-/Users/dave/code/math-dsl/maths.dsl
+class Maths
+  attr_accessor :start, :operations, :result
+
+  def initialize(start, &block)
+    self.start = start ||= 0
+    self.operations = []
+    self.result = start
+
+    instance_eval &block
+  end
+
+  def add(value)
+    self.operations << ['+', value]
+    self.result = self.result + value
+  end
+
+  def minus(value)
+    self.operations << ['-', value]
+    self.result = self.result - value
+  end
+
+  def multiply(value)
+    self.operations << ['*', value]
+    self.result = self.result * value
+  end
+
+  def to_s
+    operations_string = ""
+    operations.each do |o|
+      (operations_string << " #{o[0]} #{o[1]}")
+    end
+    "Calculation result: #{start}#{operations_string} = #{result}"
+  end
+end
+
+maths = Maths.new(2) do
+  add 3
+  add 1
+  minus 1
+  minus 1
+  multiply 2
+end
+
+puts maths.to_s
